f85b7f7cb95fecd31a8240b41cf639579707e6a4
AliArefnia <maarefnia1377@gmail.com>
2025-02-21 16:28:43 +0330

    Add animation and Accordion Form to Pending and Finished Tasks Sections

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -11,28 +11,46 @@
       />
       <BaseButton class="w-1/6" @click="addTask">Enter</BaseButton>
     </section>
-    <section class="pendingTasks">
-      <div>
-        <p class="mt-6">Pending Tasks</p>
+    <section id="pendingTasks">
+      <div
+        class="mt-6 flex justify-between cursor-pointer"
+        @click="togglePendingVisibility()"
+      >
+        <p class="text-lg">Pending Tasks</p>
+        <ChevronLeft
+          class="bg-neutral-900 w-10 h-10 rounded-full flex items-center justify-center hover:cursor-pointer transition-transform duration-300"
+          :class="{ '-rotate-90': visibelPendingTasks }"
+        />
       </div>
-      <ul>
-        <li v-for="task in pendingTasks">
-          <Tasks :task="task" :key="task.id" class="border-zinc-500"></Tasks>
-        </li>
-      </ul>
+      <Transition name="slide">
+        <ul v-if="visibelPendingTasks">
+          <li v-for="task in pendingTasks">
+            <Tasks :task="task" :key="task.id" class="border-zinc-500"></Tasks>
+          </li>
+        </ul>
+      </Transition>
     </section>
-    <section class="FinishedTasks">
-      <div>
-        <p class="mt-6">Finished Tasks</p>
+    <section id="FinishedTasks">
+      <div
+        class="flex mt-6 justify-between cursor-pointer"
+        @click="toggleFinishedVisibility()"
+      >
+        <p class="text-lg">Finished Tasks</p>
+        <ChevronLeft
+          class="bg-neutral-900 w-10 h-10 rounded-full flex items-center justify-center hover:cursor-pointer transition-transform duration-300"
+          :class="{ '-rotate-90': visibelFinishedTasks }"
+        ></ChevronLeft>
       </div>
-      <ul>
-        <li
-          v-for="task in finishedTasks"
-          class="line-through decoration-1 text-neutral-500"
-        >
-          <Tasks :task="task" :key="task.id" class="border-green-900"></Tasks>
-        </li>
-      </ul>
+      <Transition name="slide">
+        <ul v-if="visibelFinishedTasks" class="transition-all duration-300">
+          <li
+            v-for="task in finishedTasks"
+            class="line-through decoration-1 text-neutral-500"
+          >
+            <Tasks :task="task" :key="task.id" class="border-green-900"></Tasks>
+          </li>
+        </ul>
+      </Transition>
     </section>
   </div>
 </template>
@@ -55,9 +73,19 @@ export default {
     return {
       newTaskDescription: null,
       isLoading: false,
+      visibelPendingTasks: true,
+      visibelFinishedTasks: true,
     };
   },
   methods: {
+    togglePendingVisibility() {
+      this.visibelPendingTasks = !this.visibelPendingTasks;
+      console.log(this.visibelPendingTasks);
+    },
+    toggleFinishedVisibility() {
+      this.visibelFinishedTasks = !this.visibelFinishedTasks;
+      console.log(this.visibelFinishedTasks);
+    },
     event(event) {
       console.log(event);
     },
@@ -85,3 +113,25 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+.slide-enter-active,
+.slide-leave-active {
+  transition: all 0.3s ease-out;
+}
+.slide-enter-from,
+.slide-leave-to {
+  max-height: 0;
+  opacity: 0;
+  transform: translateY(-10px);
+}
+.slide-enter-to,
+.slide-leave-from {
+  max-height: 100vh;
+  opacity: 1;
+  transform: translateY(0);
+}
+/* .task-lists-move {
+  transition: transform 0.8s ease;
+} */
+</style>
