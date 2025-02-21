e644969bb9a02f20f8169b6a8de93f665f8e6e4b
AliArefnia <maarefnia1377@gmail.com>
2025-02-21 16:25:03 +0330

    Change some icons and using lucid-vue
    use lucid-vue to handle some icons

--- src/main.js
+++ src/main.js
@@ -7,13 +7,17 @@ import router from "./router/index.js";
 import BaseButton from "./components/ui/BaseButton.vue";
 import BaseTickbutton from "./components/ui/BaseTickbutton.vue";
 import BaseBinbutton from "./components/ui/BaseBinbutton.vue";
+import { Trash2, ChevronLeft } from "lucide-vue-next";
 
 const app = createApp(App);
 
 app.use(store);
 app.use(router);
 
-app.mount("#app");
 app.component("BaseButton", BaseButton);
 app.component("BaseTickButton", BaseTickbutton);
 app.component("BaseBinbutton", BaseBinbutton);
+app.component("Trash2", Trash2);
+app.component("ChevronLeft", ChevronLeft);
+
+app.mount("#app");
