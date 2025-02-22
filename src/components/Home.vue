ae04221d663deee99b0d9d0a5efb469dd78817ad
AliArefnia <maarefnia1377@gmail.com>
2025-02-22 18:28:17 +0330

    Change UI
    Change UI so scroll in long list apply only in section related to tasks and make it responsive

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,75 +1,91 @@
 <template>
-  <div>
-    <section class="flex w-full">
+  <div class="overflow-hidden">
+    <section class="flex w-full mb-6">
       <input
         type="text"
         id="task"
-        class="w-5/6 px-3 bg-zinc-700 border-b-neutral-500 rounded-md"
+        class="w-full lg:w-5/6 px-3 bg-zinc-700 border-b-neutral-500 rounded-md h-[3rem]"
         placeholder="Enter Your Task ..."
         v-model="newTaskDescription"
         @keypress.enter="addTask"
       />
-      <BaseButton class="w-1/6" @click="addTask">Enter</BaseButton>
-    </section>
-    <section id="pendingTasks">
-      <div
-        class="mt-6 flex justify-between cursor-pointer"
-        @click="togglePendingVisibility()"
+      <BaseButton class="w-1/6 hidden lg:inline-block" @click="addTask"
+        >Enter</BaseButton
       >
-        <p class="text-lg">Pending Tasks</p>
-        <ChevronLeft
-          class="bg-neutral-900 w-10 h-10 rounded-full flex items-center justify-center hover:cursor-pointer transition-transform duration-300"
-          :class="{ '-rotate-90': visibelPendingTasks }"
-        />
-      </div>
-      <Transition name="slide">
-        <ul v-if="visibelPendingTasks">
-          <li v-for="task in pendingTasks">
-            <Tasks :task="task" :key="task.id" class="border-zinc-500"></Tasks>
-          </li>
-          <p
-            v-if="pendingTasks.length === 0 && finishedTasks.length !== 0"
-            class="text-neutral-600 text-center"
-          >
-            Congrats! You Have no pending Task
-          </p>
-          <p
-            v-if="pendingTasks.length === 0 && finishedTasks.length === 0"
-            class="text-neutral-600 text-center"
-          >
-            You Havn't added any Task
-          </p>
-        </ul>
-      </Transition>
     </section>
-    <section id="FinishedTasks">
-      <div
-        class="flex mt-6 justify-between cursor-pointer"
-        @click="toggleFinishedVisibility()"
+    <article class="overflow-auto">
+      <section
+        class="content-wrapper pt-0 lg:px-[16px] lg:box-content lg:pr-[calc(16px + 1rem)] pr-4"
       >
-        <p class="text-lg">Finished Tasks</p>
-        <ChevronLeft
-          class="bg-neutral-900 w-10 h-10 rounded-full flex items-center justify-center hover:cursor-pointer transition-transform duration-300"
-          :class="{ '-rotate-90': visibelFinishedTasks }"
-        ></ChevronLeft>
-      </div>
-      <Transition name="slide">
-        <ul v-if="visibelFinishedTasks" class="transition-all duration-300">
-          <li
-            v-for="task in finishedTasks"
-            class="line-through decoration-1 text-neutral-500"
+        <section id="pendingTasks">
+          <div
+            class="flex justify-between cursor-pointer"
+            @click="togglePendingVisibility()"
           >
-            <Tasks :task="task" :key="task.id" class="border-green-900"></Tasks>
-          </li>
-          <p
-            v-if="finishedTasks.length === 0"
-            class="text-neutral-600 text-center"
+            <p class="text-lg">Pending Tasks</p>
+            <ChevronLeft
+              class="bg-neutral-900 w-10 h-10 rounded-full flex items-center justify-center hover:cursor-pointer transition-transform duration-300"
+              :class="{ '-rotate-90': visibelPendingTasks }"
+            />
+          </div>
+          <Transition name="slide">
+            <ul v-if="visibelPendingTasks">
+              <li v-for="task in pendingTasks">
+                <Tasks
+                  :task="task"
+                  :key="task.id"
+                  class="border-zinc-500"
+                ></Tasks>
+              </li>
+              <p
+                v-if="pendingTasks.length === 0 && finishedTasks.length !== 0"
+                class="text-neutral-600 text-center"
+              >
+                Congrats! You Have no pending Task
+              </p>
+              <p
+                v-if="pendingTasks.length === 0 && finishedTasks.length === 0"
+                class="text-neutral-600 text-center"
+              >
+                You Havn't added any Task
+              </p>
+            </ul>
+          </Transition>
+        </section>
+        <section id="FinishedTasks">
+          <div
+            class="flex mt-6 justify-between cursor-pointer"
+            @click="toggleFinishedVisibility()"
           >
-            You Havn't any Finished Task Yet
-          </p>
-        </ul>
-      </Transition>
-    </section>
+            <p class="text-lg">Finished Tasks</p>
+            <ChevronLeft
+              class="bg-neutral-900 w-10 h-10 rounded-full flex items-center justify-center hover:cursor-pointer transition-transform duration-300"
+              :class="{ '-rotate-90': visibelFinishedTasks }"
+            ></ChevronLeft>
+          </div>
+          <Transition name="slide">
+            <ul v-if="visibelFinishedTasks" class="transition-all duration-300">
+              <li
+                v-for="task in finishedTasks"
+                class="line-through decoration-1 text-neutral-500"
+              >
+                <Tasks
+                  :task="task"
+                  :key="task.id"
+                  class="border-green-900"
+                ></Tasks>
+              </li>
+              <p
+                v-if="finishedTasks.length === 0"
+                class="text-neutral-600 text-center"
+              >
+                You Havn't any Finished Task Yet
+              </p>
+            </ul>
+          </Transition>
+        </section>
+      </section>
+    </article>
   </div>
 </template>
 
@@ -134,10 +150,6 @@ export default {
 </script>
 
 <style scoped>
-/* .custom-chevron {
-  @apply bg-zinc-900 w-10 h-10 rounded-full flex items-center justify-center hover:cursor-pointer transition-transform duration-300;
-} */
-
 .slide-enter-active,
 .slide-leave-active {
   transition: all 0.3s ease-out;
@@ -155,7 +167,4 @@ export default {
   opacity: 1;
   transform: translateY(0) scaleY(1);
 }
-/* .task-lists-move {
-  transition: transform 0.8s ease;
-} */
 </style>
