d34d3f29795577fd72785ced22e313dc99209130
AliArefnia <maarefnia1377@gmail.com>
2025-02-24 18:36:29 +0330

    Improve UI
    1-add related Icons to Main Task lists
    2-change UI and take needed classes from index.html to app.vue

--- src/App.vue
+++ src/App.vue
@@ -20,18 +20,25 @@ export default {
 </script>
 
 <template>
-  <div class="flex w-full flex-col">
-    <header class="px-6 py-4">
-      <RouterLink v-if="notNavPage" to="/navigator" class="flex w-fit">
-        <span class="flex items-center hover:brightness-150 w-fit">
-          <ArrowLeftFromLine
-            size="30"
-            class="text-neutral-300 mr-2"
-          ></ArrowLeftFromLine>
-          <p class="text-lg text-neutral-300">Lists</p>
-        </span></RouterLink
-      >
-    </header>
+  <div
+    class="text-white m-0 w-full flex rounded-2xl overflow-hidden"
+    :class="{
+      'bg-black': !this.notNavPage,
+      'bg-priamry': this.notNavPage && this.setBcakGround,
+    }"
+  >
+    <div class="flex w-full flex-col">
+      <header class="px-6 py-4">
+        <RouterLink v-if="notNavPage" to="/navigator" class="flex w-fit">
+          <span class="flex items-center hover:brightness-150 w-fit">
+            <ArrowLeftFromLine
+              size="30"
+              class="text-neutral-300 mr-2"
+            ></ArrowLeftFromLine>
+            <p class="text-lg text-neutral-300">Lists</p>
+          </span></RouterLink
+        >
+      </header>
 
     <!-- <RouterView -->
     <RouterView class="flex flex-col w-full p-6 overflow-auto"></RouterView>
--- src/components/navigation.vue
+++ src/components/navigation.vue
@@ -1,11 +1,19 @@
 <template>
-  <div class="lg:text-center flex flex-col">
+  <div class="lg:text-center flex flex-col h-full">
     <h1 class="w-full text-6xl mb-3">Welcome</h1>
     <h2 class="w-full text-3xl text-red-500">Here is Your ToDo-List</h2>
-    <ul class="flex flex-col border-b-2 mb-3">
-      <RouterLink to="today">Today</RouterLink>
-      <RouterLink to="AllTasks">All Tasks</RouterLink>
-      <RouterLink to="importantTasks">Important Tasks</RouterLink>
+    <ul class="flex flex-col border-b-2 border-b-neutral-500 mb-3 py-3">
+      <RouterLink to="today" class="my-1">
+        <Sun class="inline mr-2" color="gold"></Sun>Today</RouterLink
+      >
+      <RouterLink to="AllTasks" class="my-1"
+        ><Infinity class="inline mr-2" color="pink"></Infinity> All
+        Tasks</RouterLink
+      >
+      <RouterLink to="importantTasks" class="my-1"
+        ><Star class="inline mr-2" color="red"></Star> Important
+        Tasks</RouterLink
+      >
     </ul>
     <TasksLists></TasksLists>
   </div>
@@ -13,9 +21,9 @@
 
 <script>
 import TasksLists from "./TasksLists.vue";
-
+import { Sun, Star, Infinity } from "lucide-vue-next";
 export default {
-  components: { TasksLists },
+  components: { TasksLists, Sun, Star, Infinity },
   created() {
     console.log(this.$route);
   },
