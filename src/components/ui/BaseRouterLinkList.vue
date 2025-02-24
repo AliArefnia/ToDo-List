2465794d58ce59b49e43cd797b27cbe36a090ed3
AliArefnia <maarefnia1377@gmail.com>
2025-02-24 14:08:10 +0330

    Create BaseRouterLinkList.vue
    1.Create BaseRouterLinkLists to be a template for lists which will added
    2.define a separate slot for icon which can be changed

---
+++ src/components/ui/BaseRouterLinkList.vue
@@ -0,0 +1,13 @@
+<template>
+  <RouterLink :to="to" class="flex my-2 mr-2 items-center rounded-md px-4 py-2">
+    <slot name="icons"><List class="mr-2" color="#b70000" /></slot>
+    <slot></slot>
+  </RouterLink>
+</template>
+<script>
+import { List } from "lucide-vue-next";
+export default {
+  components: { List },
+  props: ["to"],
+};
+</script>
