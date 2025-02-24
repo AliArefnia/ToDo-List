1bdb163d5ce05e56b821538df4d7730e33db7daa
AliArefnia <maarefnia1377@gmail.com>
2025-02-24 14:13:52 +0330

    Update TasksLists.vue
    1-use BseRouterLinkList instead of simple routerlink
    2-change UI

--- src/components/TasksLists.vue
+++ src/components/TasksLists.vue
@@ -1,28 +1,35 @@
 <template>
-  <div>
-    <RouterLink
-      :to="`/taskLists/${tasklist.name}`"
-      v-for="tasklist in tasksLists"
-      :key="tasklist.name"
-      :list="tasklist.name"
-      >{{ tasklist.name }}</RouterLink
-    >
-    <input
-      type="text"
-      id="task"
-      class="w-full lg:w-5/6 px-3 bg-zinc-700 border-b-neutral-500 rounded-md h-[3rem]"
-      placeholder="Enter Your List name here ..."
-      v-model="newListName"
-      @keypress.enter="addList"
-    />
+  <div class="flex flex-col overflow-auto">
+    <!-- <section class="flex flex-col overflow-auto basis-3/4"> -->
+    <section class="flex flex-col overflow-auto basis-3/4">
+      <BaseRouterLinkList
+        :to="`/taskLists/${tasklist.name}`"
+        v-for="tasklist in tasksLists"
+        :key="tasklist.name"
+        :list="tasklist.name"
+        >{{ tasklist.name }}</BaseRouterLinkList
+      >
+    </section>
+    <!-- <section class="overflow-hidden mt-2 basis-1/4"> -->
+    <section class="overflow-hidden mt-2 grow">
+      <input
+        type="text"
+        id="task"
+        class="w-full lg:w-5/6 px-3 bg-zinc-700 border-b-neutral-500 rounded-md h-[40px]"
+        placeholder="Enter Your List name here ..."
+        v-model="newListName"
+        @keypress.enter="addList"
+      />
+    </section>
   </div>
 </template>
 
 <script>
 import TaskItems from "./TaskItems.vue";
+import BaseRouterLinkList from "./ui/BaseRouterLinkList.vue";
 
 export default {
-  components: { TaskItems },
+  components: { TaskItems, BaseRouterLinkList },
   data() {
     return {
       isLoading: false,
